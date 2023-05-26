befb31728e2447336c11f8b67b4b3abd7ec99ac8
Bats34 <amanbatra793@gmail.com>
2023-05-26 15:00:06 +0530

    right circle

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftWristx=0;
 leftWristy=0;
 rightWristy=0;
 scoreLeft=0;
+scoreRight=0;
+songStatus2="";
 songStatus="";
 song="";
 song2="";
@@ -21,14 +23,24 @@ function setup() {
 function draw() {
     image(video,0,0,600,500);
     songStatus=song.isPlaying();
+    songStatus2=song2.isPlaying();
     if(scoreLeft>0.2) {
-        stroke("black");
-        fill("red");
+        stroke("red");
+        fill("black");
         circle(leftWristx,leftWristy,20);
         song2.stop();
         if(songStatus==false) {
             song.play();
             document.getElementById("status").innerHTML="Harry Potter Theme Song Playing!";
+        }
+    }else if(scoreRight>0.2) {
+        stroke("black");
+        fill("red");
+        circle(rightWristx,rightWristy,20);
+        song.stop();
+        if(songStatus2==false) {
+            song2.play();
+            document.getElementById("status").innerHTML="Peter Pan Song Playing!"
         }
     }
 }
@@ -43,5 +55,6 @@ function Gotposes(results) {
         rightWristx=results[0].pose.rightWrist.x;
         rightWristy=results[0].pose.rightWrist.x;
         scoreLeft=results[0].pose.keypoints[9].score;
+        scoreRight=results[0].pose.keypoints[10].score;
     }
 }
